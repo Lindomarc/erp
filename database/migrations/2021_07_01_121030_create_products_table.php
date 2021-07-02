0472e0ee37294ce2cf5e9358980fa32a793819de
lindomarc <lindomarc@outlook.com>
2021-07-02 14:29:27 -0300

    fix: migrate

--- database/migrations/2021_07_01_121030_create_products_table.php
+++ database/migrations/2021_07_01_121030_create_products_table.php
@@ -18,11 +18,11 @@ class CreateProductsTable extends Migration
 	        $table->string('name',150);
 	        $table->string('slug',170);
 	        $table->string('product_code',20)->nullable()->default('');
-	        $table->boolean('is_product')->default(false);
-	        $table->boolean('is_material')->default(false);
+	        $table->boolean('is_product')->default(0);
+	        $table->boolean('is_material')->default(0);
 	        $table->decimal('price_buy')->unsigned()->default(0);
 	        $table->decimal('price_sale')->unsigned()->default(0);
-	        $table->boolean('status')->default(true);
+	        $table->boolean('status')->default(1);
 	
 	        $table->integer('category_id')->unsigned();
 	        $table->foreign('category_id')->references('id')->on('categories');
